ab33d7b0112fd970ef399dd592a73ab3bdaadac6
BasantaKumar Sahoo <basantakumar.sahoo@capgemini.com>
2021-11-09 00:33:11 +0000

    Minor Layout Changes

--- OneCloudTransformer/Startup.cs
+++ OneCloudTransformer/Startup.cs
@@ -59,7 +59,7 @@ namespace OneCloudTransformer
                 endpoints.MapControllerRoute(
                      name: "default",
                      pattern: "{controller}/{action}/{id?}",
-                     defaults: new { controller = "Pricing", action = "ProductList" });
+                     defaults: new { controller = "Home", action = "Index" });
             });
         }
     }
